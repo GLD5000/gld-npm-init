7a2996e77f2c2dc31f6b7bf64dfebbecda8a37cb
Gareth Devlin <garethlouisdevlin@gmail.com>
2025-07-14 09:05:48 +0100

    Update spawn

--- src/npmInitSpawn.mjs
+++ src/npmInitSpawn.mjs
@@ -1,13 +1,8 @@
 import { spawnSync } from "node:child_process";
-import { fileURLToPath } from "url";
-import path from "path";
-// const __filename = fileURLToPath(import.meta.url); // get the resolved path to the file
-// const __dirname = path.dirname(__filename);
 
 export function npmInitSpawn() {
-  spawnSync("npm", ["init"], {
+  spawnSync("npm init", {
     shell: true,
-    // cwd: __dirname,
     detached: true,
     stdio: "ignore",
   });
